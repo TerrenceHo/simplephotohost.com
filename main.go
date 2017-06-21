756df6e2fdd92bb8a291c69b820de0bc0d2fc161
Terrence Ho <terrenceho.books@gmail.com>
2017-06-20 22:42:59 -0700

    Added fresh config file

--- main.go
+++ main.go
@@ -6,7 +6,8 @@ import (
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1>This site is driven by machine learning</h1>")
+	fmt.Println("Someone visted our page")
+	fmt.Fprint(w, "<h1>This site is driven by super awesome machine learning</h1>")
 }
 
 func main() {
