bc874db86d4ae153e54f54cc7cbc40bc17186c6d
Terrence Ho <terrenceho.books@gmail.com>
2017-06-21 00:27:47 -0700

    Practiced Routing

--- main.go
+++ main.go
@@ -6,8 +6,12 @@ import (
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Someone visted our page")
-	fmt.Fprint(w, "<h1>This site is driven by super awesome machine learning</h1>")
+	w.Header().Set("Content-Type", "text/html")
+	if r.URL.Path == "/" {
+		fmt.Fprint(w, "<h1>This site is driven by super awesome machine learning</h1>")
+	} else if r.URL.Path == "/contact" {
+		fmt.Fprint(w, "To get in touch, please send an email to <a href=\"mailto:support@lenslocked.com\">support@lenslocked.com</a>")
+	}
 }
 
 func main() {
