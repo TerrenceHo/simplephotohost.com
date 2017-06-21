30b59742005303424a1fcddfaec0346b1829afcb
Terrence Ho <terrenceho.books@gmail.com>
2017-06-21 07:36:30 -0700

    Added 404 and used status code

--- main.go
+++ main.go
@@ -11,6 +11,9 @@ func handlerFunc(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "<h1>This site is driven by super awesome machine learning</h1>")
 	} else if r.URL.Path == "/contact" {
 		fmt.Fprint(w, "To get in touch, please send an email to <a href=\"mailto:support@lenslocked.com\">support@lenslocked.com</a>")
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprint(w, "<h1>We could not find the page you were looking for.  Oops! Try again maybe?</h1>")
 	}
 }
 
