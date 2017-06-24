d265a3bfe8f18cd813eab0cc45bb1daeab7ed7e2
Terrence Ho <terrenceho.books@gmail.com>
2017-06-23 22:40:05 -0700

    Created POST request for signup page.  Signup for updated, main updated, users.go updated

--- controllers/users.go
+++ controllers/users.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"fmt"
 	"net/http"
 
 	"lenslocked.com/views"
@@ -20,8 +21,16 @@ type Users struct {
 	NewView *views.View
 }
 
+// New is used to render the form where the user can create a new user account
+// GET /signup is path
 func (u *Users) New(w http.ResponseWriter, r *http.Request) {
 	if err := u.NewView.Render(w, nil); err != nil {
 		panic(err)
 	}
+}
+
+// Create is used to process signup form when a user submits it
+// POST /signup
+func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "This is temp.")
 }
