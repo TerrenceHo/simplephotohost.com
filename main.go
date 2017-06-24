0e7834210891b43dce5b443357bda9dabca19d32
Terrence Ho <terrenceho.books@gmail.com>
2017-06-23 21:54:55 -0700

    -Refactored some code to solve a bug where there signup form did not render

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"net/http"
+
+	"lenslocked.com/controllers"
+	"lenslocked.com/views"
+
+	"github.com/gorilla/mux"
+)
+
+var (
+	homeView    *views.View
+	contactView *views.View
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	must(homeView.Render(w, nil))
+}
+
+func contact(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	must(contactView.Render(w, nil))
+}
+
+func main() {
+	homeView = views.NewView("bootstrap", "views/home.gohtml")
+	contactView = views.NewView("bootstrap", "views/contact.gohtml")
+	usersC := controllers.NewUsers()
+
+	r := mux.NewRouter()
+	r.HandleFunc("/", home)
+	r.HandleFunc("/contact", contact)
+	r.HandleFunc("/signup", usersC.New)
+	http.ListenAndServe(":3000", r)
+}
+
+func must(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
