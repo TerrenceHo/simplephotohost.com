bb9a03585521600d288e57023760245f832b284d
Terrence Ho <terrenceho.books@gmail.com>
2017-06-26 20:00:11 -0700

    -Created models/users.go -Created UserServices struct -Created Users sturct -Created func to open db connection -Created func to query user from database -Created func to drop and create db if necessary

--- models/users.go
+++ models/users.go
@@ -1,9 +1,61 @@
 package models
 
 import (
+	"errors"
+
 	"github.com/jinzhu/gorm"
-	_ "github.github.com/jinzhu/gorm/dialect/postgres"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
+
+var (
+	// ErrNotFound is returned when resource cannot be found in database
+	ErrNotFound = errors.New("models: resource not found")
+)
+
+func NewUserService(connectionInfo string) (*UserService, error) {
+	db, err := gorm.Open("postgres", connectionInfo)
+	if err != nil {
+		return nil, err
+	}
+	// defer db.Close()
+	db.LogMode(true)
+	return &UserService{
+		db: db,
+	}, nil
+}
+
+type UserService struct {
+	db *gorm.DB
+}
+
+// Look up user by the ID provided
+// 1 - user, nil
+// 2 - nil, ErrNotFound
+// 3 - nil, OtherError
+func (us *UserService) ByID(id int) (*User, error) {
+	var user User
+	err := us.db.Where("id = ?", id).First(&user).Error
+	switch err {
+	case nil:
+		return &user, nil
+	case gorm.ErrRecordNotFound:
+		return nil, ErrNotFound
+	default:
+		return nil, err
+	}
+
+}
+
+// Closes db connnection
+func (us *UserService) Close() error {
+	return us.db.Close()
+}
+
+// Drops user table and rebuilds it
+func (us *UserService) DestructiveReset() {
+	us.db.DropTableIfExists(&User{})
+	us.db.AutoMigrate(&User{})
+}
 
 type User struct {
 	gorm.Model
