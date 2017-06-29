7eb4e8d51640708e21f2ea5e73c74d7fd515c0ee
Terrence Ho <terrenceho.books@gmail.com>
2017-06-28 17:08:48 -0700

    -Created rand package to help create random strings -Created Bytes function that generates n random bytes -Created String function that returns a string of the base64 URL encoded version of that byte slice -RememberToken() is a helper function designed to generate remember tokens of a predetermined byte slice

---
+++ rand/strings.go
@@ -0,0 +1,35 @@
+package rand
+
+import (
+	"crypto/rand"
+	"encoding/base64"
+)
+
+const RememberTokenBytes = 32
+
+// Bytes will help generate n random bytes, or returns an error if there was
+// one., This uses the crypto/rand package so it is safe to generate tokens
+func Bytes(n int) ([]byte, error) {
+	b := make([]byte, n)
+	_, err := rand.Read(b)
+	if err != nil {
+		return nil, err
+	}
+	return b, nil
+}
+
+// String will generate a byte slice of size nBytes and then return a string
+// that is the base64 URL encoded version of that byte slice
+func String(nBytes int) (string, error) {
+	b, err := Bytes(nBytes)
+	if err != nil {
+		return "", err
+	}
+	return base64.URLEncoding.EncodeToString(b), nil
+}
+
+// RememberToken is a helper function designed to generate remember tokens of a
+// predetermined byte slice
+func RememberToken() (string, error) {
+	return String(RememberTokenBytes)
+}
