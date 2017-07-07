5ae35d9cd64b0dd2f49d88f0f59a7d5dac9cb9a0
Terrence Ho <terrenceho.books@gmail.com>
2017-07-06 22:54:22 -0700

    controllers/galleries.go -Created Update function that will update the title of the gallery main.go -Created route for galleries/update views/galleries/edit.gohtml -Fixed bug

--- controllers/galleries.go
+++ controllers/galleries.go
@@ -66,6 +66,30 @@ func (g *Galleries) Edit(w http.ResponseWriter, r *http.Request) {
 	g.EditView.Render(w, vd)
 }
 
+// POST /galleries/:id/update
+func (g *Galleries) Update(w http.ResponseWriter, r *http.Request) {
+	gallery, err := g.galleryByID(w, r)
+	if err != nil {
+		return
+	}
+	user := context.User(r.Context())
+	if gallery.UserID != user.ID {
+		http.Error(w, "Gallery Not Found", http.StatusNotFound)
+		return
+	}
+	var vd views.Data
+	vd.Yield = gallery
+	var form GalleryForm
+	if err := parseForm(r, &form); err != nil {
+		log.Println(err)
+		vd.SetAlert(err)
+		g.EditView.Render(w, vd)
+	}
+	gallery.Title = form.Title
+	fmt.Fprintln(w, gallery)
+	// g.EditView.Render(w, vd)
+}
+
 // POST /galleries
 func (g *Galleries) Create(w http.ResponseWriter, r *http.Request) {
 	var vd views.Data
