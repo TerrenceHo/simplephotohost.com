70cb805adccef759bf67e9ce428b975965374e70
Terrence Ho <terrenceho.books@gmail.com>
2017-07-06 23:06:45 -0700

    controllers/galleries.go -Added success alert to Update func models/galleries.go -Created Update method which runs validation checks then updates the gallery. -Created Update fun which updates the gallery

--- controllers/galleries.go
+++ controllers/galleries.go
@@ -86,6 +86,17 @@ func (g *Galleries) Update(w http.ResponseWriter, r *http.Request) {
 		g.EditView.Render(w, vd)
 	}
 	gallery.Title = form.Title
+	err = g.gs.Update(gallery)
+	if err != nil {
+		vd.SetAlert(err)
+		g.EditView.Render(w, vd)
+		return
+	}
+	vd.Alert = &views.Alert{
+		Level:   views.AlertLvlSuccess,
+		Message: "Gallery Update Successful!",
+	}
+	g.EditView.Render(w, vd)
 	fmt.Fprintln(w, gallery)
 	// g.EditView.Render(w, vd)
 }
